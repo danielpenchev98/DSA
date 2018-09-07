eae653e4770dcc3ee8740718551b8db063d6d2e4
danielpenchev98 <42358431+danielpenchev98@users.noreply.github.com>
2018-09-07 20:05:40 +0300

    Update GraphLinkedList.h

--- Graph/GraphLinkedList.h
+++ Graph/GraphLinkedList.h
@@ -525,6 +525,19 @@ inline bool GraphLinkedList<T>::CheckForInComingEdges(const std::string& name) c
 	return 0;
 }
 //topological sort for finding a cycle in a graph
+//L ← Empty list that will contain the sorted elements
+//G ← Set of all nodes with no incoming edge
+//while Q is non-empty do
+//  remove a node n from Q
+//   add n to tail of L
+//    for each node m with an edge e from n to m do
+  //      remove edge e from the graph
+  //      if m has no other incoming edges then
+  //          insert m into Q
+//if graph has edges then
+  //  return error   (graph has at least one cycle)
+//else 
+  //  return L  
 template<typename T>
 inline bool GraphLinkedList<T>::isCyclic() const
 {
