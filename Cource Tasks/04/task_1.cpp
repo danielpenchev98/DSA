7486687db1ae305cc893c12a411b9caba3945196
Daniel Penchev <42358431+danielpenchev98@users.noreply.github.com>
2019-03-17 19:47:25 +0200

    Update task_1.cpp

--- Cource Tasks/04/task_1.cpp
+++ Cource Tasks/04/task_1.cpp
@@ -1,1 +1,45 @@
+#include <cmath>
+#include <cstdio>
+#include <vector>
+#include <iostream>
+#include <algorithm>
+struct Node
+{
+    int data;
+    Node* next;
+};
 
+int main() {
+  //  LinkedList list;
+    int num = 0;
+    std::ios_base::sync_with_stdio(false);
+    std::cin>>num;
+    Node* temp=new Node{num,nullptr};
+    Node* prev=temp;
+    Node* start=temp;
+    while (std::cin>>num)
+    {
+        temp=new Node{num,nullptr};
+        prev->next=temp;
+        prev=temp;
+    }
+    temp->next=nullptr;
+    int min = INT32_MAX;
+    int max = INT32_MIN;
+    long long sum = 0;
+    while(start!=nullptr)
+    {
+        if(min>start->data)
+        {
+            min=start->data;
+        }
+        if(max<start->data)
+        {
+            max=start->data;
+        }
+        sum+=start->data;
+        start=start->next;
+    }
+    std::cout << min << " " << max << " " << sum << std::endl;
+    return 0;
+}
